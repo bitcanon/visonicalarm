48f08ead3533ba60819a48b1a71dd60dc15299c7
Mikael Schultz <mikael@dofiloop.com>
2018-07-04 14:14:23 +0200

    Preparation for PyPI

--- setup.py
+++ setup.py
@@ -4,18 +4,18 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="visonic",
+    name="visonicalarm",
     version="1.0.3",
     author="Mikael Schultz",
     author_email="mikael@dofiloop.com",
     description="A simple library for the Visonic Alarm API.",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/MikaelSchultz/visonic",
+    url="https://github.com/MikaelSchultz/visonicalarm",
     packages=setuptools.find_packages(),
-    classifiers=(
+    classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
-    ),
+    ],
 )
