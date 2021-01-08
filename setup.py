0b083cd583e9dee2bc41f8229235bdcd1eaf799b
Mikael Schultz <mikael@bitcanon.com>
2021-01-08 09:26:15 +0100

    Change package version for PyPI

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r', encoding='utf-8') as fh:
 
 setuptools.setup(
     name='visonicalarm',
-    version='2.0.0',
+    version='2.0.1',
     author='Mikael Schultz',
     author_email='mikael@bitcanon.com',
     description='A simple library for the Visonic Alarm API written in Python 3.',
