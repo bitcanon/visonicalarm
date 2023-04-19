70d9d1e8a3ce6cfe8941e91b54961302b1a788e5
Mark Parker <msparker@sky.com>
2023-04-20 00:57:48 +0100

    v4.0.0b0

--- setup.py
+++ setup.py
@@ -1,23 +1,23 @@
 import setuptools
 
-with open('README.md', 'r', encoding='utf-8') as fh:
+with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name='visonicalarm',
-    version='3.12.0',
-    author='Mikael Schultz',
-    author_email='bitcanon@pm.me',
-    description='A simple library for the Visonic Alarm API written in Python 3.',
+    name="visonicalarm",
+    version="4.0.0b0",
+    author="Mikael Schultz",
+    author_email="bitcanon@pm.me",
+    description="A simple library for the Visonic Alarm API written in Python 3.",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/bitcanon/visonicalarm',
+    long_description_content_type="text/markdown",
+    url="https://github.com/bitcanon/visonicalarm",
     packages=setuptools.find_packages(),
-    install_requires=['requests', 'python-dateutil'],
+    install_requires=["requests", "python-dateutil"],
     classifiers=[
-        'Programming Language :: Python :: 3',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
     ],
-    python_requires='>=3.6',
+    python_requires=">=3.6",
 )
